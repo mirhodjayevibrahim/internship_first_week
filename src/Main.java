b88067c42f53fcc6f09bdeb2dd70af42cb6504cc
Ibrahim Mirhodjayev <mirhodjayevibrahim@gmail.com>
2026-04-29 14:41:21 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        if(i % 3 == 0 && i % 5 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else if(i % 3 == 0){
+            System.out.println("Fizz");
+        }
+        else if(i % 5 == 0){
+            System.out.println("Buzz");
+        }
+
+
+    }
+}
